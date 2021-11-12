b4af153530d2932e09d29556a2baea5a32239daf
Daneliz Urena <danelizurena@danelizs-mbp.myfiosgateway.com>
2021-11-11 22:09:57 -0500

    mern 10

--- frontend/src/screens/homeScreen.js
+++ frontend/src/screens/homeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
-const homeScreen = () => {
+const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+      
+        const fetchProducts = async () => {
+            const {data}  = await axios.get('/api/products')
+
+            setProducts(data) 
+        }
+
+        fetchProducts()
+    }, [])
     return (
         <>
            <h1>Latest Products</h1> 
@@ -18,4 +30,4 @@ const homeScreen = () => {
     )
 }
 
-export default homeScreen
+export default HomeScreen
