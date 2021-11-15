9b38f1eff31c010823e63675264f1d90be105d73
Daneliz Urena <danelizurena@Danelizs-MacBook-Pro.local>
2021-11-15 14:11:24 -0500

    12

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
 
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
-      <Nav className='ml-auto'>
+      <Nav className='ms-auto'>
 
       <LinkContainer to ='/cart' >
         <Nav.Link to='/cart'><i className = ' fas fa-shopping-cart' ></i>Cart</Nav.Link>
